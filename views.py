428a8ea0845394bd7e5b1b3532ed8c39a0d589ad
rem_zi <97393585+reabs3301@users.noreply.github.com>
2025-02-28 15:18:46 +0100

    Update views.py

--- views.py
+++ views.py
@@ -34,17 +34,22 @@ def searching(request):
 
     if request.method == 'POST':
         to_search = request.POST['input_to_pass']
-        if to_search[0] == ':':
-            
-            category_to_search = to_search[ 1 : to_search.find('/')]
-            if len(to_search[to_search.find('/')+1 : ].strip()) == 0:
-                returned_items = {'products_returned' : product.objects.filter(categorie = category_to_search) , 'searched' : to_search[to_search.find('/')+1 : ]}
-            else:
-                returned_items = {'products_returned' : product.objects.filter(categorie = category_to_search , name__contains = to_search[to_search.find('/')+1 : ]) , 'searched' : to_search[to_search.find('/')+1 : ]}
-            return render(request , "search.html" , returned_items)
-        elif to_search[0] != ':':
-            returned_items = {'products_returned' : product.objects.filter(name__contains = to_search) , 'searched' : to_search}
-            return render(request , "search.html" , returned_items)
-    return render(request , "search.html" , {'msg' : "you must type something"})
+        if not to_search :
+            not_passed = True
+            return render(request , 'search.html', {'not_passed' : not_passed , 'msg' : 'you need to type something to search'})
+        else :
+            if to_search[0] == ':':
+                
+                category_to_search = to_search[ 1 : to_search.find('/')]
+                if len(to_search[to_search.find('/')+1 : ].strip()) == 0:
+                    returned_items = {'products_returned' : product.objects.filter(categorie = category_to_search) , 'searched' : to_search[to_search.find('/')+1 : ]}
+                else:
+                    returned_items = {'products_returned' : product.objects.filter(categorie = category_to_search , name__contains = to_search[to_search.find('/')+1 : ]) , 'searched' : to_search[to_search.find('/')+1 : ]}
+                return render(request , "search.html" , returned_items)
+            elif to_search[0] != ':':
+                returned_items = {'products_returned' : product.objects.filter(name__contains = to_search) , 'searched' : to_search}
+                return render(request , "search.html" , returned_items)
+        return render(request , "search.html" , {'msg' : "you must type something"})
+
 
 
